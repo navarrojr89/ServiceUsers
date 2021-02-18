2ceaf83855538dfcda23c62cf581989edb270bce
Luis Navarro <navarrojr89@gmail.com>
2021-02-18 17:40:30 +0100

    Starting with service users to handle authentication

---
+++ src/main/java/com/lamn/microservices/serviceusers/UsersServiceApplication.java
@@ -0,0 +1,21 @@
+package com.lamn.microservices.serviceusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Users service application.
+ */
+@SpringBootApplication
+public class UsersServiceApplication {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(UsersServiceApplication.class, args);
+	}
+
+}
